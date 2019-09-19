5592e83122c1e21db65802a426d7e27afc453d7a
彭石桂 <gui950823@126.com>
2019-09-19 15:05:25 +0800

    LGTeachingMaterialFramework 1.0.0.0

---
+++ LGTeachingMaterialFramework/Classes/LGTeachingMaterialFramework.framework/Headers/LGTeachingMaterialVersion.h
@@ -0,0 +1,14 @@
+//
+//  LGTeachingMaterialVersion.h
+//  LGTeachingMaterialFramework
+//
+//  Created by SG on 2019/9/19.
+//  Copyright © 2019 刘亚军. All rights reserved.
+//
+
+#ifndef LGTeachingMaterialVersion_h
+#define LGTeachingMaterialVersion_h
+
+/** 版本：1.0.0.0    2019-9-19 */
+
+#endif /* LGTeachingMaterialVersion_h */
