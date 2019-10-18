c1339c703daa651ad3c665ba2bc8bf56851c6c20
彭石桂 <gui950823@126.com>
2019-10-18 15:38:05 +0800

    1.0.0.2

--- LGTeachingMaterialFramework/Classes/LGTeachingMaterialFramework.framework/Headers/LGTeachingMaterialVersion.h
+++ LGTeachingMaterialFramework/Classes/LGTeachingMaterialFramework.framework/Headers/LGTeachingMaterialVersion.h
@@ -9,6 +9,9 @@
 #ifndef LGTeachingMaterialVersion_h
 #define LGTeachingMaterialVersion_h
 
+/** 版本：1.0.0.2    2019-10-18 */
+/** 1、完成iPad适配 */
+
 /** 版本：1.0.0.1    2019-9-24 */
 /** 1、解决pod引用冲突问题 */
 
